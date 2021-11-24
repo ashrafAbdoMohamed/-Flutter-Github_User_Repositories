061e8ddc0725edc0a9376009218f3dc51619ede3
ashrafAbdoMohamed <ashrefabdo150@gmail.com>
2021-11-24 19:40:23 +0200

    upload

--- lib/screens/user_search_screen.dart
+++ lib/screens/user_search_screen.dart
@@ -36,38 +36,40 @@ class _UserSearchScreenState extends State<UserSearchScreen> {
         backgroundColor: primaryBackgroundColor,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // text field label
-            text("Enter your Github username" , fontSize: textSizeLargeMedium, ),
-            10.height,
-            // text field
-            textField(hint: "Username", controller: usernameController,
-              keyboardType: TextInputType.name, width: width * 0.7,),
-            10.height,
-            // the search button (as long as there's an api call, the button is hidden and an animation widget will be present)
-            // to prevent the user from clicking more than one time, hence, calling the api more than once at a time.
-            // this button calls the getUserRepos() method in the controller and when the request is successfully returned
-            // we navigate to the RepositoriesListScreen to represent them
-            Obx( () => (reposController.isLoading.value)
-                ? LoadingAnimation()
-                : appButton("Search", (){
-                  if(usernameController.text.isEmpty){
-                    snackBar(context , title: "Please enter a name!");
-                  }else{
-                    reposController.getUserRepos(context, usernameController.text ).then((value) {
-                      if(value != null){
-                        Navigator.push(context, MaterialPageRoute(builder: (c) => RepositoriesListScreen(usernameController.text) ));
-                      }else{
-                        snackBar(context , title: "Username is not found");
-                      }
-                    });
-                  }
-              } , btnWidth: width * 0.5,),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // text field label
+              text("Enter your Github username" , fontSize: textSizeLargeMedium, ),
+              10.height,
+              // text field
+              textField(hint: "Username", controller: usernameController,
+                keyboardType: TextInputType.name, width: width * 0.7,),
+              10.height,
+              // the search button (as long as there's an api call, the button is hidden and an animation widget will be present)
+              // to prevent the user from clicking more than one time, hence, calling the api more than once at a time.
+              // this button calls the getUserRepos() method in the controller and when the request is successfully returned
+              // we navigate to the RepositoriesListScreen to represent them
+              Obx( () => (reposController.isLoading.value)
+                  ? LoadingAnimation()
+                  : appButton("Search", (){
+                    if(usernameController.text.isEmpty){
+                      snackBar(context , title: "Please enter a name!");
+                    }else{
+                      reposController.getUserRepos(context, usernameController.text ).then((value) {
+                        if(value != null){
+                          Navigator.push(context, MaterialPageRoute(builder: (c) => RepositoriesListScreen(usernameController.text) ));
+                        }else{
+                          snackBar(context , title: "Username is not found");
+                        }
+                      });
+                    }
+                } , btnWidth: width * 0.5,),
+              ),
+            ],
+          ),
         ),
       ),
     );
